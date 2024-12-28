9cb1cd50595541642b590d2e7d6489d56edb2c5c
Patrik <praprotnik.patrik@gmail.com>
2024-12-28 23:04:17 +0100

    Refactoring & Adding reconstructing path functionality

--- Vaja11/Vaja11.cpp
+++ Vaja11/Vaja11.cpp
@@ -5,185 +5,286 @@
 #include <chrono>
 using namespace std;
 
-int stevilo_vozlisc;
+int number_of_nodes;
+string fileName = "graph1.txt";
 
-struct pot {
-	int iz_vozlisca, v_vozlisce, cena;
-	bool* S = new bool[stevilo_vozlisc];
-	pot* naslednji;
+// Structure to represent a path in the graph
+struct Path {
+    int from_node = 0, to_node = 0, cost = 0;               // From which node, to which node, and the cost of the path
+    bool* visited = new bool[number_of_nodes];  // Array to track visited nodes
+    Path* next = nullptr;                                 // Pointer to the next path in the list
 };
 
 void menu() {
-	cout << "Trgovski potnik - izbira:" << endl;
-	cout << "1) Preberi matriko" << endl;
-	cout << "2) Resi problem trgovskega potnika" << endl;
-	cout << "3) Izpisi dobljen seznam nivojev" << endl;
-	cout << "4) Rekunstrukcija poti" << endl;
-	cout << "5) Konec" << endl;
-	cout << endl;
-	cout << "Izbira: ";
+    cout << endl;
+    cout << "Traveling Salesperson Problem - Options:" << endl;
+    cout << "1) Read matrix" << endl;
+    cout << "2) Solve the Traveling Salesperson Problem" << endl;
+    cout << "3) Display generated levels" << endl;
+    cout << "4) Reconstruct the path" << endl;
+    cout << "5) Exit" << endl;
+    cout << endl;
+    cout << "Choice: ";
 }
 
-void branje(int* C[]) {
+// Read the graph matrix from a file
+void readMatrix(int* C[]) {
+    ifstream file(fileName);
 
-	ifstream f("graf1.txt");
-	f >> stevilo_vozlisc;
+    // Check if the file opened successfully
+    if (!file.is_open()) {
+        cout << "Error: Could not open the file '" << fileName << "'." << endl;
+        return;
+    }
 
-	for (int i = 0; i < stevilo_vozlisc; i++) {
-		C[i] = new int[stevilo_vozlisc];
-	}
+    // Try reading the number of nodes from the file
+    if (!(file >> number_of_nodes)) {
+        cout << "Error: Could not read the number of nodes from the file. Please check the file format." << endl;
+        return;
+    }
 
-	int cena_povezave;
-	for (int i = 0; i < stevilo_vozlisc; i++) {
-		for (int j = 0; j < stevilo_vozlisc; j++) {
-			f >> cena_povezave;
-			C[i][j] = cena_povezave;
-		}
-	}
+    // Initialize the adjacency matrix
+    for (int i = 0; i < number_of_nodes; i++) {
+        C[i] = new int[number_of_nodes];
+    }
 
-	//for (int i = 0; i < stevilo_vozlisc; i++) {
-	//	for (int j = 0; j < stevilo_vozlisc; j++) {
-	//		std::cout << C[i][j] << " ";
-	//	}
-	//	cout << endl;
-	//}
-}
+    // Fill the adjacency matrix with costs from the file
+    int connection_cost;
+    for (int i = 0; i < number_of_nodes; i++) {
+        for (int j = 0; j < number_of_nodes; j++) {
+            if (!(file >> connection_cost)) { // Check if reading the cost is successful
+                cout << "Error: Could not read the connection cost for node (" << i << ", " << j
+                    << "). Please check the file format." << endl;
+                return;
+            }
+            C[i][j] = connection_cost;
+        }
+    }
 
-vector<pot*> vstavi_prva_vozlisca(int* C[]) {
-	vector <pot*> prve_poti;
-
-	for (int i = 1; i < stevilo_vozlisc; i++) {
-		pot *p = new pot();
-		p->iz_vozlisca = i + 1;			//poti označim, da gredo od 1 do stevilo_vozlisc (čeprav so indeksi od 0 do stevilo_vozlisc-1)
-		p->v_vozlisce = 1;
-		p->cena = C[i][0];
-		for (int j = 0; j < stevilo_vozlisc; j++) {
-			p->S[j] = false;
-		}
-
-		if(!prve_poti.empty())
-			prve_poti.back()->naslednji = p;
-
-		prve_poti.push_back(p);
-	}
-
-	return prve_poti;
-}
-
-void napravi_seznam(int* C[], int vozlisce, stack <vector<pot*>> sklad, vector <pot*> &nivo) {
-	vector <pot*> prejsnji_nivo = sklad.top();
-
-	for (int i = 0; i < prejsnji_nivo.size(); i++) {
-		if (prejsnji_nivo[i]->iz_vozlisca == vozlisce)		//če obe poti izhajata iz istega vozlišča, ignoriramo
-			continue;
-		if (!prejsnji_nivo[i]->S[vozlisce]) {				//če še podanega vozlišča ni v množici povezav... 
-			pot *p = new pot();								//ustvarimo novo pot na podlagi poti prejsnjega nivoja in podanega vozlišča
-			p->iz_vozlisca = vozlisce;
-			p->v_vozlisce = prejsnji_nivo[i]->iz_vozlisca;
-			p->cena= C[p->iz_vozlisca][p->v_vozlisce] + prejsnji_nivo[i]->cena;
-			p->S = prejsnji_nivo[i]->S;
-			p->S[p->v_vozlisce - 1] = true;					//za novo ustvarjeno pot označimo v množici da obstaja
-
-			nivo.push_back(p);								//pot dodamo na nivo
-		}
-	}
-
-	for (int i = 0; i < nivo.size(); i++) {							//preverimo če se kateri izmed poti v novem nivoju ujemata v "iz_vozlisca" in S
-		for (int j = i+1; j < nivo.size(); i++) {
-			if (nivo[i]->iz_vozlisca == nivo[j]->iz_vozlisca && nivo[i]->S == nivo[j]->S) {		//... če se, odstranimo dražjo
-				if (nivo[i]->cena > nivo[j]->cena)
-					nivo.erase(nivo.begin() + i);
-				else
-					nivo.erase(nivo.begin() + j);
-			}
-		}
-
-	}
-
-}
-
-stack <vector<pot*>> potnik(int* C[]) {
-	auto start = std::chrono::steady_clock::now();
-
-	stack <vector<pot*>> sklad;
-
-	vector <pot*> seznam_poti;
-	seznam_poti = vstavi_prva_vozlisca(C);						//ustvarimo prvi nivo za skladu
-	sklad.push(seznam_poti);									//... in ga damo na sklad
-
-	for (int i = 2; i < stevilo_vozlisc - 1; i++) {							//sprehodimo se če vse ostale nivoje (razen zadnjega):
-		vector <pot*> nivo;													//ustvarimo prazen "nivo"
-		for (int vozlisce = 2; vozlisce < stevilo_vozlisc; vozlisce++) {
-			napravi_seznam(C, vozlisce, sklad, nivo);						//napolnimo nivo (ustvarimo seznam) z vsemi povezavami od 2. do predzadnjega vozlišča
-		}
-		sklad.push(nivo);
-	}
-
-	seznam_poti.clear();				
-	napravi_seznam(C, 1, sklad, seznam_poti);					//ustvarimo zadnji nivo (seznam poti) in ga dodamo na sklad
-	sklad.push(seznam_poti);
-
-	auto end = std::chrono::steady_clock::now();
-	cout << "Cas trajanja algoritma: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "μs." << endl;
-
-	return sklad;
+    // Notify the user that the matrix was read successfully
+    cout << "Matrix successfully read from the file '" << fileName << "'." << endl;
 }
 
 
-void izpis_nivojev(stack <vector<pot*>> sklad) {
-	vector <pot*> nivo;
-	int stevec = 0;
+// Create initial paths starting from the first node
+vector<Path*> createInitialPaths(int* C[]) {
+    vector<Path*> initial_paths;
 
-	while(sklad.size() > 0) {
-		nivo = sklad.top();
+    for (int i = 1; i < number_of_nodes; i++) {
+        Path* p = new Path();
+        p->from_node = i + 1;       // Start from node 1 to all other nodes
+        p->to_node = 1;             // End at node 1
+        p->cost = C[i][0];          // Cost from the current node to node 1
 
-		cout << "NIVO " << sklad.size() - stevec << ".:" << endl;
-		for (int j = 0; j < nivo.size(); j++) {
-			cout << "iz: " << nivo[j]->iz_vozlisca << ", v: " << nivo[j]->v_vozlisce << ", cena: " << nivo[j]->cena << endl;
-		}
-		cout << endl;
+        // Mark all nodes as unvisited initially
+        for (int j = 0; j < number_of_nodes; j++) {
+            p->visited[j] = false;
+        }
 
-		sklad.pop();
-	}
+        // Add the new path to the list
+        if (!initial_paths.empty()) {
+            initial_paths.back()->next = p;
+        }
+
+        initial_paths.push_back(p);
+    }
+
+    return initial_paths;
+}
+
+// Generate the level list of paths for the given node
+void generateLevelList(int* C[], int current_node, stack<vector<Path*>>& path_stack, vector<Path*>& level) {
+    vector<Path*> previous_level = path_stack.top();
+
+    // Iterate over paths in the previous level
+    for (Path* prev_path : previous_level) {
+        // Skip paths that originate from the same node
+        if (prev_path->from_node == current_node) continue;
+
+        // If the current node is not visited in the path
+        if (!prev_path->visited[current_node]) {
+            // Create a new path
+            Path* p = new Path();
+            p->from_node = current_node;
+            p->to_node = prev_path->from_node;
+            p->cost = C[p->from_node - 1][p->to_node - 1] + prev_path->cost;
+
+            // Copy the visited status and mark the new node as visited
+            p->visited = prev_path->visited;
+            p->visited[p->to_node - 1] = true;
+
+            level.push_back(p); // Add the new path to the current level
+        }
+    }
+
+    // Remove redundant paths with higher costs
+    for (size_t i = 0; i < level.size(); i++) {
+        for (size_t j = i + 1; j < level.size(); j++) {
+            if (level[i]->from_node == level[j]->from_node && level[i]->visited == level[j]->visited) {
+                if (level[i]->cost > level[j]->cost) {
+                    level.erase(level.begin() + i);
+                }
+                else {
+                    level.erase(level.begin() + j);
+                }
+            }
+        }
+    }
+}
+
+// Solve the Traveling Salesperson Problem
+stack<vector<Path*>> solveTSP(int* C[]) {
+    auto start = chrono::steady_clock::now();
+
+    stack<vector<Path*>> path_stack;
+
+    // Generate the first level of paths and add it to the stack
+    vector<Path*> first_level = createInitialPaths(C);
+    path_stack.push(first_level);
+
+    // Generate all levels except the last
+    for (int i = 2; i < number_of_nodes - 1; i++) {
+        vector<Path*> level;                                // Create an empty level
+        for (int node = 2; node < number_of_nodes; node++) {
+            generateLevelList(C, node, path_stack, level);  // Populate the level
+        }
+        path_stack.push(level);                             // Add the level to the stack
+    }
+
+    // Generate the final level of paths
+    vector<Path*> last_level;
+    generateLevelList(C, 1, path_stack, last_level); // Connect back to the starting node
+    path_stack.push(last_level);
+
+    auto end = chrono::steady_clock::now();
+    cout << "Algorithm execution time: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << "μs." << endl;
+
+    return path_stack;
+}
+
+// Display all levels of paths
+void displayLevels(stack<vector<Path*>> path_stack) {
+    vector<Path*> level;
+    int level_counter = 0;
+
+    while (!path_stack.empty()) {
+        level = path_stack.top();
+
+        cout << "LEVEL " << path_stack.size() - level_counter << ":" << endl;
+        for (Path* p : level) {
+            cout << "From: " << p->from_node << ", To: " << p->to_node << ", Cost: " << p->cost << endl;
+        }
+        cout << endl;
+
+        path_stack.pop();
+    }
+}
+
+// Reconstruct the optimal path for the Traveling Salesman Problem
+void reconstructPath(stack<vector<Path*>> levels) {
+    if (levels.empty()) {
+        cout << "Error: No levels to reconstruct the path." << endl;
+        return;
+    }
+
+    // Get the last level from the stack
+    vector<Path*> lastLevel = levels.top();
+    levels.pop();
+
+    // Find the path with the minimum cost in the last level
+    Path* optimalPath = nullptr;
+    int minimumCost = INT_MAX;
+
+    for (Path* path : lastLevel) {
+        if (path->cost < minimumCost) {
+            minimumCost = path->cost;
+            optimalPath = path;
+        }
+    }
+
+    if (!optimalPath) {
+        cout << "Error: No valid path found in the last level." << endl;
+        return;
+    }
+
+    // Trace back the path from the optimal path to the start
+    vector<int> reconstructedPath;
+    int currentNode = optimalPath->from_node;
+    reconstructedPath.push_back(currentNode);
+
+    while (!levels.empty()) {
+        vector<Path*> currentLevel = levels.top();
+        levels.pop();
+
+        for (Path* path : currentLevel) {
+            if (path->to_node == currentNode) {
+                currentNode = path->from_node;
+                reconstructedPath.push_back(currentNode);
+                break;
+            }
+        }
+    }
+
+    // Add the start node to complete the cycle
+    reconstructedPath.push_back(1);
+
+    // Reverse the path to display it in the correct order
+    reverse(reconstructedPath.begin(), reconstructedPath.end());
+
+    // Output the reconstructed path
+    cout << "Reconstructed Path: ";
+    for (int node : reconstructedPath) {
+        cout << node << " ";
+    }
+    cout << endl;
+
+    cout << "Total Path Cost: " << optimalPath->cost << endl;
 }
 
 
 int main() {
-	int selection;
-	bool running = true;
+    int selection;
+    bool running = true;
 
-    int** C = new int* [stevilo_vozlisc];
-	stack <vector<pot*>> sklad;
+    // Adjacency matrix for the graph
+    int** C = new int* [number_of_nodes];
+    stack<vector<Path*>> path_stack;
 
-	do {
-		menu();
-		cin >> selection;
-		switch (selection) {
-		case 1:
-			branje(C);
-			cout << endl;
-			break;
-		case 2:
-			sklad = potnik(C);
-			cout << endl;
-			break;
-		case 3:
-			cout << endl;
-			izpis_nivojev(sklad);
-			cout << endl;
-			break;
-		case 4:
+    do {
+        menu();
+        cin >> selection;
+        system("cls");
+        switch (selection) {
+        case 1:
+            readMatrix(C);
+            cout << endl;
+            break;
+        case 2:
+            path_stack = solveTSP(C);
+            cout << endl;
+            break;
+        case 3:
+            cout << endl;
+            displayLevels(path_stack);
+            cout << endl;
+            break;
+        case 4:
+            if (path_stack.empty()) {
+                cout << "Error: No levels to reconstruct the path. Solve the problem first (Case 2)." << endl;
+            }
+            else {
+                reconstructPath(path_stack);
+            }
+            break;
+            break;
+        case 5:
+            running = false;
+            break;
+        default:
+            cout << "ERROR: Invalid selection." << endl;
+            break;
+        }
+    } while (running);
 
-			break;
-		case 5:
-			running = false;
-			break;
-		default:
-			cout << "ERROR";
-			break;
-		}
-	} while (running);
-
-	return 0;
+    return 0;
 }
-
